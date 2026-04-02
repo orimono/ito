da75e4a291d0381636e279d7c2b752803cbc97ca
nifnil <finn@fnli.in>
2026-04-02 22:56:26 +0800

    feat: update TaskManifest type in JoinPacket to use Capability struct

--- go.mod
+++ go.mod
@@ -3,6 +3,6 @@ module github.com/orimono/ito
 go 1.25.8
 
 require (
-	github.com/denisbrodbeck/machineid v1.0.1 // indirect
+	github.com/denisbrodbeck/machineid v1.0.1
 	golang.org/x/sys v0.42.0 // indirect
 )
--- packet.go
+++ packet.go
@@ -1,5 +1,11 @@
 package ito
 
+type Capability struct {
+	Version   string            `json:"version"`
+	Params    map[string]string `json:"params"`
+	Platforms []string          `json:"platforms"`
+}
+
 type JoinPacket struct {
 	NodeID   string `json:"node_id"`
 	Hostname string `json:"hostname"`
@@ -7,7 +13,7 @@ type JoinPacket struct {
 	OS   string `json:"os"`
 	Arch string `json:"arch"`
 
-	TaskManifest map[string]string `json:"task_manifest"`
+	TaskManifest map[string]Capability `json:"task_manifest"`
 
 	Version string   `json:"version"`
 	Tags    []string `json:"tags"`
