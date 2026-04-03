84a4b81a6b439666ccf71cad452c2a625f953751
nifnil <finn@fnli.in>
2026-04-03 11:57:16 +0800

    feat: add MemoryMetrics struct to track memory usage statistics

--- metrics.go
+++ metrics.go
@@ -8,4 +8,8 @@ type Telemetry struct {
 }
 
 type MemoryMetrics struct {
+	Total       uint64  `json:"total"`
+	Used        uint64  `json:"used"`
+	Free        uint64  `json:"free"`
+	UsedPercent float64 `json:"used_percent"`
 }
